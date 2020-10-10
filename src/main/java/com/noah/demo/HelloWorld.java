a2860d39990debf74743e5d6791278565a0b2554
yinzongchang <yinzongchang@meicai.cn>
2020-10-10 23:43:53 +0800

    secord

--- src/main/java/com/noah/demo/HelloWorld.java
+++ src/main/java/com/noah/demo/HelloWorld.java
@@ -4,9 +4,15 @@ package com.noah.demo;
  * Title: HelloWorld.java <br>
  * Description:             <br>
  * Copyright: Copyright (c) 2015<br>
- * Company: 北京云杉世界信息技术有限公司<br>
  *
  * @author yinzo 2020/10/10
  */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hehe");
+    }
+
 }
