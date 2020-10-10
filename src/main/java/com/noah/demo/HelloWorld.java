c5a4c323e5666da3f6af1c5d3cf28fdf45c12c24
yinzongchang <yinzongchang@meicai.cn>
2020-10-10 23:18:15 +0800

    first commit

---
+++ src/main/java/com/noah/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.noah.demo;
+
+/**
+ * Title: HelloWorld.java <br>
+ * Description:             <br>
+ * Copyright: Copyright (c) 2015<br>
+ * Company: 北京云杉世界信息技术有限公司<br>
+ *
+ * @author yinzo 2020/10/10
+ */
+public class HelloWorld {
+}
